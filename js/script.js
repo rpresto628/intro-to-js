c68f0e0728a813c8e88ed1572795fbdc9b32aebd
Rose Presto <presto-r@kcusd.dist>
2019-04-23 18:59:10 -0700

    commiting beginning code for js. Computer died...

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// number data type
+// every variable (var) comand must be followed by a console.log command in order to initiate.
+var a =10;
+console.log(a);
+var b =100;
+console.log(b);
+var a =20; // can use whole numbers
+console.log(a);
+var c =0x1E;  // hex value 30
+console.log(c);
+var d = 0.5;  // can also use decimals for values
+console.log(d);
+
+var sum = a + b;
+console.log(sum); // will show 120. It will take the b and last a portion 100+20=120
+
+
+
+// subtraction
+var diff = d-c;
+console.log(diff); // will show -29.5. It is subtracting 0.5-0x1E(30)=-29.5
+
+// multiplication
+var mult = a*d;
+console.log(mult);
+
+// division
+var div = a / d;
+console.log(div);
+
+var div = b / a;
+console.log(div);
+
+// modulus
+var mod = b % a;
+console.log(mod);
+var mod = b % c;
+console.log(mod);
+
+var e = '5';
+console.log(e);
+
